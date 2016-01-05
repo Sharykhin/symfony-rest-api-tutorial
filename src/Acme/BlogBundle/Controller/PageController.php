0c3f9f18f9dc5130eb4a4e00bdd3eb6c8ca5ade2
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2016-01-05 10:23:36 +0300

    remove useless namespace and come to part 2

--- src/Acme/BlogBundle/Controller/PageController.php
+++ src/Acme/BlogBundle/Controller/PageController.php
@@ -4,7 +4,6 @@ namespace Acme\BlogBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PageController extends FOSRestController
 {
